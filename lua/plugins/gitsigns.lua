9d336328f1968ecb7e43c460c0461ce14b62a6b6
Vishesh <vishesh.singh@pocketfm.com>
2025-05-24 16:07:25 +0530

    completions and fugitive

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,12 @@
 return {
+  {"tpope/vim-fugitive"},
+  {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup()
-    vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
     vim.keymap.set('n', "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
     vim.keymap.set('n', '<leader>hr', ":Gitsigns reset_hunk<CR>", {})
   end,
+    },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
  "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "pyright", "tsserver"}
+        ensure_installed = {"lua_ls", "pyright", "ts_ls"}
       })
     end
   },
@@ -16,10 +16,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.pyright.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.ts_ls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
