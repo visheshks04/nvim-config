5509f45331eaa7ecc04cc33996f585996c5a0fd5
Vishesh <vishesh.singh@pocketfm.com>
2025-06-14 02:37:07 +0530

    oil and a few extra keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,3 +13,10 @@ vim.opt.laststatus=0
 
 vim.api.nvim_set_keymap("n", "<Space>", "Nop", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<Space>", "Nop", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<C-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
+vim.keymap.set("n", "<leader>t", ":70vsplit | term<CR>") -- open a terminal on left
