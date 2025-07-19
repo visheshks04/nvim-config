400021bdef721eca100a64f0b87211dca5b9ba47
Vishesh <visheshkrsinghofficial@gmail.com>
2025-07-20 00:32:40 +0530

    plugin cleanup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,7 +16,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.pyright.setup({})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,32 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-		vim.keymap.set("n", "<C-f><C-f>", builtin.live_grep, {})
-	end,
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          width = 0.9,           -- overall width: 90% of screen
+          height = 0.9,          -- overall height: 80% of screen
+          horizontal = {
+            preview_cutoff = 0,  -- always show preview
+            preview_width = 0.7, -- preview pane uses ~70%
+          },
+          vertical = {
+            preview_cutoff = 0,
+          },
+        },
+      },
+    })
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+    vim.keymap.set("n", "<C-f><C-f>", builtin.live_grep, {})
+    vim.keymap.set("n", "<C-q>", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>com", builtin.git_commits, {})
+    vim.keymap.set("n", "<leader>bra", builtin.git_branches, {})
+    vim.keymap.set("n", "<leader>sta", builtin.git_status, {})
+    vim.keymap.set("n", "<leader>sh", builtin.git_stash, {})
+  end,
 }
