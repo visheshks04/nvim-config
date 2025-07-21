c9fc13a498da7b6e391be16647fec0d3a66e2de9
Vishesh <visheshkrsinghofficial@gmail.com>
2025-07-22 01:14:40 +0530

    floating oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,7 +9,15 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   config = function()
-    require("oil").setup()
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory with Oil" })
+    require("oil").setup({
+      -- Your custom options here
+      view_options = {
+        show_hidden = true, -- Show hidden files
+      },
+      win_options = {
+        wrap = false, -- Disable line wrapping
+      },
+    })
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory with Oil" })
   end,
 }
