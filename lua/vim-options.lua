10ca2baa4f11bf2fdec0854839e5b6124663f602
Vishesh <visheshkrsinghofficial@gmail.com>
2026-03-13 23:55:07 +0530

    diisable smear cursor

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -23,8 +23,6 @@ vim.opt.winblend=50
 vim.api.nvim_set_keymap("n", "<Space>", "Nop", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<Space>", "Nop", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<C-j>", ":m .+1<CR>==") -- move line up(n)
-vim.keymap.set("n", "<C-k>", ":m .-2<CR>==") -- move line down(n)
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 
